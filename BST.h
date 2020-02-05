df16b72446a0cd61e84d9ac4971da29c3617d607
unknown <thapaachyut8863>
2020-02-05 19:11:23 +0545

    complete

--- BST.h
+++ BST.h
@@ -1,7 +1,11 @@
-class BST
-{
+#ifndef BST_h
+#define BST_h
+
+class BST{
 	public:
-	virtual void add(int data) = 0;
-	virtual void preorder_traversal() = 0;
-	virtual void search(int data) = 0;
+		virtual void add(int data)=0;
+		virtual void preOrderTraversal(int index)=0;
+		virtual bool search(int data)=0;
 };
+
+#endif
