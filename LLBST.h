a96f6f28a6f11bfa55e87f540aed189364bfbc76
Hritik thapa <57740801+HritikThapa7@users.noreply.github.com>
2020-02-06 09:00:06 +0545

    complete

---
+++ LLBST.h
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "BST.h"
+
+class Node{
+
+	public:
+		int info;
+		Node* left;
+		Node* right;
+
+};
+
+class node{
+	public:
+		Node* info;
+		node* next;
+};
+
+class LLBST : public BST{	
+	public:	
+	Node* root;
+	LLBST();
+	void add(int data);
+	void preOrderTraversal();
+	bool search(int data);
+	bool isEmpty();
+};
+
+class Stack{
+	public:
+		Stack();
+		~Stack();
+
+		void push(Node* a);
+		Node* pop();
+		bool isEmpty();
+		bool isFull();
+		int peak();
+
+	private:
+		node* stack;
+		node* top;
+
+};
